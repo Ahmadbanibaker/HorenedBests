d5ea1ff6c8ef1447f9605f0e12c5089f016b674a
Ahmadbanibaker <ahmadmurad502@gmail.com>
2021-09-06 15:42:04 +0300

    added img and likes

--- src/component/HornedBeasts.js
+++ src/component/HornedBeasts.js
@@ -1,13 +1,26 @@
 import React, { Component } from 'react';
 
  class HornedBeasts extends Component {
+     constructor(props){
+         super(props);
+         this.state={
+             stateLikes:0,
+         }
+
+     }
+     raiseLikes=()=>{
+         this.setState({
+             stateLikes:this.state.stateLikes+1
+         })
+     }
     render() {
         return (
             <div>
                 <h2> title :  {this.props.title} </h2>
                <img src = {this.props.imageUrl} alt={this.props.title} title='animal image'/>
                <p> description : {this.props.description} </p>
-               <button> Likes </button>
+               <button onClick={this.raiseLikes}> Likes </button>
+               <h2> {this.state.stateLikes}</h2>
             </div>
         )
     }
